903dddeb3a221d3807c0943b858978a74938521c
snsk <shinsk@gmail.com>
2013-03-31 22:21:04 +0900

    sync comment logs

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -43,7 +43,7 @@ function change(message){
 function addComment(msg){
     var str = $(msg).val();
     nicoscreen.add(msg);
-    $("#text").prev().append('<div id="user">'+ msg +'</div>');
+    $("#text").next().append('<div id="user">'+ msg +'</div>');
     $("#msg").val("");
 }
 
